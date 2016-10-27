8ec1a5ceb26071cf6157c73ff3680f60f1e268d5
George King <george.w.king@gmail.com>
2016-10-27 13:31:15 -0400

    remove ancient 'common' dir.'

--- common/all.py
+++
@@ -1,8 +0,0 @@
-# Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/ by George King.
-
-from functools import singledispatch
-
-from .fs import *
-from .io import *
-from .subproc import *
-from .util import *
