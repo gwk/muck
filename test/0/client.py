0f22da7b45d22dc8193de51a93a26613ba7fa12e
George King <george.w.king@gmail.com>
2017-02-14 17:50:53 -0500

    test/0/client.

---
+++ test/0/client.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from utest import *
+import muck
+
+
+def test(load_many_stream):
+  for file, args in load_many_stream:
+    yield file.read(), args
+
+exp = [('load-many_0.\n', {'i':0}), ('load-many_1.\n', {'i':1})]
+
+utest_seq(exp, test, muck.load_many('client/load-many_{i}.txt', i=[0, 1]))
+utest_seq(exp, test, muck.load_many('client/load-many_{i}.txt', i={0, 1}))
+utest_seq(exp, test, muck.load_many('client/load-many_{i}.txt', i=(0, 1)))
+utest_seq(exp, test, muck.load_many('client/load-many_{i}.txt', i=range(2)))
--- test/0/multi/c-many.txt.py
+++ test/0/multi/c-many.txt.py
@@ -3,25 +3,15 @@ from utest import *
 import muck
 
 
-
 def test(load_many_stream):
   for file, args in load_many_stream:
     yield file.read(), args
 
 a_exp = [('a-to-many 0\n', {'i':0}), ('a-to-many 1\n', {'i':1})]
-
-utest_seq(a_exp, test, muck.load_many('a-to-many-{i}.txt', i=[0, 1]))
-utest_seq(a_exp, test, muck.load_many('a-to-many-{i}.txt', i={0, 1}))
-utest_seq(a_exp, test, muck.load_many('a-to-many-{i}.txt', i=(0, 1)))
-utest_seq(a_exp, test, muck.load_many('a-to-many-{i}.txt', i=range(0, 2)))
-
-utest_val('a-to-many 0\n', muck.load('a-to-many-0.txt').read())
-
 b_exp = [('b-through 0\n', {'i':0}), ('b-through 1\n', {'i':1})]
 
-utest_seq(b_exp, test, muck.load_many('b-through-{i}.txt', i=[0, 1]))
-utest_seq(b_exp, test, muck.load_many('b-through-{i}.txt', i={0, 1}))
-utest_seq(b_exp, test, muck.load_many('b-through-{i}.txt', i=(0, 1)))
-utest_seq(b_exp, test, muck.load_many('b-through-{i}.txt', i=range(0, 2)))
+utest_seq(a_exp, test, muck.load_many('a-to-many-{i}.txt', i=range(2)))
+utest_seq(b_exp, test, muck.load_many('b-through-{i}.txt', i=range(2)))
 
+utest_val('a-to-many 0\n', muck.load('a-to-many-0.txt').read())
 utest_val('b-through 0\n', muck.load('b-through-0.txt').read())
