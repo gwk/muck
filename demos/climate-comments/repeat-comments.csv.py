e92424c14ee4cd838c6c0abf9bad48709074c838
George King <george.w.king@gmail.com>
2017-03-09 10:59:19 -0500

    move demos to separate repo.

--- demos/climate-comments/repeat-comments.csv.py
+++
@@ -1,7 +0,0 @@
-import muck
-from pithy.csv_utils import *
-from pithy.io import *
-
-rows = muck.load('comment-counts.json')
-repeated = [row for row in rows if row[0] > 1]
-out_csv(header=('Count', 'Comment Text'), rows=repeated)
