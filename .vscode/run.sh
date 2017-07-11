d5b5c56182d12fffdac9d38b7173efd3b350b4ec
George King <george.w.king@gmail.com>
2017-07-11 19:54:27 -0400

    update vscode tasks to use mypy-plumage and gloss mypy problemMatcher.

--- .vscode/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
-
-# Hack around vscode's lame task runner. Simply runs the argument vector.
-
-set -e
-"$@"
-echo "done: $@"
