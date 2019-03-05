983f56283246d26494d11e73ec959855085c861b
George King <george.w.king@gmail.com>
2019-03-05 14:00:51 -0800

    comment.

--- test/interpose/run-open.sh
+++ test/interpose/run-open.sh
@@ -8,7 +8,7 @@
 # In this test, we use `which` to choose the executable like `env` would, and then provide it directly.
 # If it has SIP, then `open.py` will notice the missing variable and fail.
 
-lldb='/Applications/Xcode.app/Contents/Developer/usr/bin/lldb' # Do not use /usr/bin/lldb.
+lldb='/Applications/Xcode.app/Contents/Developer/usr/bin/lldb' # Do not use `/usr/bin/lldb`.
 python3="$(which python3)"
 
 set -x
