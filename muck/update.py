dd62c22b285f3db19be903ddfc4fb5d928f37137
George King <george.w.king@gmail.com>
2019-08-08 11:49:11 -0400

    add "directory does not exist" note to diagnose incorrect dependencies.

--- muck/update.py
+++ muck/update.py
@@ -133,6 +133,7 @@ def update_target_status(ctx:Ctx, fifo:AsyncLineReader, target:str, dpdt:Dpdt, f
   if is_product:
     target_dir = path_dir(target)
     if target_dir and not path_exists(target_dir, follow=True):
+      note(target, f'directory does not exist: {target_dir}')
       # Not possible to find a source; must be the contents of a built directory (or an erroneous dependency).
       update_target(ctx, fifo=fifo, target=target_dir, dpdt=dpdt.sub(kind='directory contents', target=target), force=force)
       if not target_status.is_updated: # build of parent did not create this product.
